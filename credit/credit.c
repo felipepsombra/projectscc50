43881963eb7daca07e3df463dbedae8e730bbebd
Felipe Pinheiro <130610742+felipepsombra@users.noreply.github.com>
2023-06-03 23:39:17 -0300

    Update credit.c

--- credit/credit.c
+++ credit/credit.c
@@ -1,64 +1,18 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
+
+bool is_valid_checksum(const char *card_number);
+string get_card_brand(const char *card_number);
 
 int main(void)
 {
-    // Request credit card number
-    long card_number = get_long("Número: ");
+    string card_number = get_string("Number: ");
 
-    // Check the length of the card number
-    int length = 0;
-    long temp = card_number;
-    while (temp != 0)
+    if (is_valid_checksum(card_number))
     {
-        temp /= 10;
-        length++;
-    }
-
-    // Check leading digits to determine card type
-    int first_two_digits = (int) (card_number / 1e12);
-    int first_digit = (int) (card_number / 1e13);
-
-    // Check leading digits to determine card type
-    int sum = 0;
-    int digit_count = 1;
-    while (card_number != 0)
-    {
-        int digit = card_number % 10;
-
-        if (digit_count % 2 == 0)
-        {
-            int product = digit * 2;
-            sum += product % 10 + product / 10;
-        }
-        else
-        {
-            sum += digit;
-        }
-
-        card_number /= 10;
-        digit_count++;
-    }
-
-    // Check the card type and print the result
-    if (sum % 10 == 0)
-    {
-        if ((length == 15 && (first_two_digits == 34 || first_two_digits == 37)))
-        {
-            printf("AMEX\n");
-        }
-        else if (length == 16 && (first_two_digits >= 51 && first_two_digits <= 55))
-        {
-            printf("MASTERCARD\n");
-        }
-        else if ((length == 13 || length == 16) && first_digit == 4)
-        {
-            printf("VISA\n");
-        }
-        else
-        {
-            printf("INVALID\n");
-        }
+        string card_brand = get_card_brand(card_number);
+        printf("%s\n", card_brand);
     }
     else
     {
@@ -66,4 +20,65 @@ int main(void)
     }
 
     return 0;
+}
+
+// Check if the checksum of the card number is valid
+bool is_valid_checksum(const char *card_number)
+{
+    int length = strlen(card_number);
+    int sum = 0;
+    bool multiply = false;
+
+    // Iterate through the card number digits from right to left
+    for (int i = length - 1; i >= 0; i--)
+    {
+        int digit = card_number[i] - '0';
+
+        if (multiply)
+        {
+            digit *= 2;
+
+            // If the doubled digit is greater than 9, add its digits separately
+            if (digit > 9)
+            {
+                digit = digit % 10 + digit / 10;
+            }
+        }
+
+        sum += digit;
+        multiply = !multiply;
+    }
+
+    // The card number is valid if the sum is divisible by 10
+    return sum % 10 == 0;
+}
+
+// Determine the brand of the card based on the card number format
+string get_card_brand(const char *card_number)
+{
+    int length = strlen(card_number);
+
+    if ((length == 13 || length == 16) && card_number[0] == '4')
+    {
+        return "VISA";
+    }
+    else if (length == 16)
+    {
+        int prefix = (card_number[0] - '0') * 10 + (card_number[1] - '0');
+        if (prefix >= 51 && prefix <= 55)
+        {
+            return "MASTERCARD";
+        }
+    }
+    else if (length == 15)
+    {
+        int prefix = (card_number[0] - '0') * 10 + (card_number[1] - '0');
+        if (prefix == 34 || prefix == 37)
+        {
+            return "AMEX";
+        }
+    }
+
+    // If none of the conditions match, the card number is invalid
+    return "INVALID";
 }
